39adf3e1c8d755ea651744f4a4f3981a30b44b53
riad02 <riadahmmed64@gmail.com>
2025-07-27 15:33:58 +0600

    Project: Band Name Generator

---
+++ Band Name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+City = input("Which city did you grow up in ?\n")
+ex_name = input("What is Your EX gf Name?\n")
+print("Your band name could be:" + City + " " + ex_name)
