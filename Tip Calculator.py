fd216e826f522c59c14a2f0c78019d6003cb7ccf
riad02 <riadahmmed64@gmail.com>
2025-07-27 20:57:10 +0600

    Project: BMI Calculator

---
+++ Tip Calculator.py
@@ -0,0 +1,5 @@
+# Calculate the bmi using weight and height.
+height = float(input("What is your height?\n"))
+weight = float(input("what is your weight?\n"))
+bmi = weight / (height ** 2)
+print("Your BMI is:", bmi)
